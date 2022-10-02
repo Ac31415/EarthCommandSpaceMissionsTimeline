20d409501ffe565638700e3f44a4c18813e83100
Byron Torres <byleto@gmail.com>
2022-10-02 22:35:55 +0200

    Update header in main page

--- src/App.js
+++ src/App.js
@@ -4,28 +4,33 @@ import { Timeline } from './Timeline/Timeline'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <h1>Team Name: Earth Mission</h1>
-
-        <img
-          src={process.env.PUBLIC_URL + '/whiteLogo.png'}
-          className="App-logo"
-          alt="logo"
-        />
-
+      <header className="header">
+        <h1 className="title">Team Name: Earth Mission</h1>
+      </header>
+      <section>
+        <Timeline />
         <h2>Team members:</h2>
         <ul>
-          <li><h5>List Item 1</h5></li>
-          <li><h5>List Item 1</h5></li>
-          <li><h5>List Item 1</h5></li>
-          <li><h5>List Item 1</h5></li>
-          <li><h5>List Item 1</h5></li>
+          <li>
+            <h5>Natalia Bulaieva</h5>
+          </li>
+          <li>
+            <h5>Byron Torres</h5>
+          </li>
+          <li>
+            <h5>Shaurya Sareen</h5>
+          </li>
+          <li>
+            <h5>Samar AboulEla</h5>
+          </li>
+          <li>
+            <h5>Wen-Chung Cheng</h5>
+          </li>
+          <li>
+            <h5>Lily MacFaydian</h5>
+          </li>
         </ul>
-
-      </header>
-
-      <Timeline />
+      </section>
     </div>
   );
 }
