7bf74a6b2f145290f5bac23103f3911383feef2d
Ac31415 <wcheng3@fau.edu>
2022-10-02 01:15:41 -0400

    adjusted positions of team name and members name template

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <h1>Team Name: Earth Command</h1>
+
         <img
           src={process.env.PUBLIC_URL + '/whiteLogo.png'}
           className="App-logo"
           alt="logo"
         />
+
+        <h2>Team members:</h2>
+        <ul>
+          <li><h5>List Item 1</h5></li>
+          <li><h5>List Item 1</h5></li>
+          <li><h5>List Item 1</h5></li>
+          <li><h5>List Item 1</h5></li>
+          <li><h5>List Item 1</h5></li>
+        </ul>
+
       </header>
-
-      <h2>Team Name: Earth Command</h2>
-      <h3>Team members</h3>
-
-      <ul>
-        <li><h5>List Item 1</h5></li>
-        <li><h5>List Item 1</h5></li>
-        <li><h5>List Item 1</h5></li>
-        <li><h5>List Item 1</h5></li>
-        <li><h5>List Item 1</h5></li>
-      </ul>
 
       <Timeline />
     </div>
